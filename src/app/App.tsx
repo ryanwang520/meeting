24f53f6fe238dbd62f547cf634980a4de0fa1251
Ryan Wang <hwwangwang@gmail.com>
2024-03-30 09:04:54 +0800

    create project

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from '@/components/ModeToggle';
+
+export default function Home() {
+  return (
+    <div>
+      <ModeToggle />
+      <div className="bg-primary text-primary-foreground">Hello</div>
+    </div>
+  );
+}
